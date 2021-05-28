a4e93e1a77ca5e8b011c5ab8667b42c9367df75b
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-05-28 18:38:37 +0200

    Funcion recibe un array y devuelve objeto con los datos requeridos

--- js/main.js
+++ js/main.js
@@ -1,68 +1,79 @@
-const pacientes = [
-  {
-    paciente: {
-      nombre: "Manuel",
-      edad: 25,
-      sexo: "H",
+const pacientes = [{
+        paciente: {
+            nombre: "Manuel",
+            edad: 25,
+            sexo: "H",
+        },
+        diasIngresado: 3,
+        dieta: "Baja en fibra",
     },
-    diasIngresado: 3,
-    dieta: "Baja en fibra",
-  },
-  {
-    paciente: {
-      nombre: "Marta",
-      edad: 56,
-      sexo: "M",
+    {
+        paciente: {
+            nombre: "Marta",
+            edad: 56,
+            sexo: "M",
+        },
+        diasIngresado: 5,
+        dieta: "Diabetes",
     },
-    diasIngresado: 5,
-    dieta: "Diabetes",
-  },
-  {
-    paciente: {
-      nombre: "Julia",
-      edad: 38,
-      sexo: "M",
+    {
+        paciente: {
+            nombre: "Julia",
+            edad: 38,
+            sexo: "M",
+        },
+        diasIngresado: 1,
+        dieta: "Sin sal",
     },
-    diasIngresado: 1,
-    dieta: "Sin sal",
-  },
-  {
-    paciente: {
-      nombre: "Esteban",
-      edad: 40,
-      sexo: "H",
+    {
+        paciente: {
+            nombre: "Esteban",
+            edad: 40,
+            sexo: "H",
+        },
+        diasIngresado: 3,
+        dieta: "Diabetes",
     },
-    diasIngresado: 3,
-    dieta: "Diabetes",
-  },
-  {
-    paciente: {
-      nombre: "Arturo",
-      edad: 19,
-      sexo: "H",
+    {
+        paciente: {
+            nombre: "Arturo",
+            edad: 19,
+            sexo: "H",
+        },
+        diasIngresado: 2,
+        dieta: "Baja en fibra",
     },
-    diasIngresado: 2,
-    dieta: "Baja en fibra",
-  },
-  {
-    paciente: {
-      nombre: "Isabel",
-      edad: 63,
-      sexo: "M",
+    {
+        paciente: {
+            nombre: "Isabel",
+            edad: 63,
+            sexo: "M",
+        },
+        diasIngresado: 6,
+        dieta: "Sin sal",
     },
-    diasIngresado: 6,
-    dieta: "Sin sal",
-  },
 ];
 const nMayoresEdad = (pacientes) =>
-  pacientes.filter((...obj) => {
-    const { paciente } = obj;
-    return paciente;
-  }).length;
-const analisisPacientes = (pacientes) => {
-  const analisis = {};
-  analisis.nPacientes = pacientes.length;
-  analisis.nMayoresEdad = nMayoresEdad(pacientes);
-  return analisis;
-};
+    pacientes.filter(({ paciente } = ficha) => paciente.edad >= 18).length;
+const nHombresDiabeticos = (pacientes) =>
+    pacientes.filter(
+        (ficha) => ficha.paciente.sexo === "H" && ficha.dieta === "Diabetes"
+    ).length;
+const totalDiasIngreso = (pacientes) =>
+    pacientes.reduce((acumulador, ficha) => acumulador + ficha.diasIngresado, 0);
+const mediaEdadMujeres = (pacientes) =>
+    pacientes
+    .filter(({ paciente } = ficha) => paciente.sexo === "M")
+    .reduce(
+        (acumulador, { paciente } = ficha, i, ficha) =>
+        acumulador + paciente.edad / ficha.length,
+        0
+    );
+const analisisPacientes = (pacientes) => ({
+    nPacientes: pacientes.length,
+    nMayoresEdad: nMayoresEdad(pacientes),
+    nHombresDiabeticos: nHombresDiabeticos(pacientes),
+    totalDiasIngreso: totalDiasIngreso(pacientes),
+    mediaEdadMujeres: mediaEdadMujeres(pacientes),
+});
 console.log(analisisPacientes(pacientes));
